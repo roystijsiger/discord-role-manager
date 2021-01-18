de9107fefd05d54cf6e4828893d4f54e5360364f
Roy-delcom <roy@delcom.nl>
2021-01-18 17:28:15 +0100

    Hotfix russian roulette

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ bot.on('message', msg => {
   if(msg.content.startsWith('!russianRoulette')){
       msg.channel.send("Lets see whose gonna die.")
       const list = bot.guilds.get("706146210876096645");
-      list.members.random();
 
       setTimeout(() => {
         msg.channel.send("1...")
@@ -28,7 +27,9 @@ bot.on('message', msg => {
       }, 3000);
 
       setTimeout(()=> {
-        msg.channel.send(`${list.members.random().nickname} haha you're dead :D`);
+        //msg.channel.send(`${list.members.random().nickname} haha you're dead :D`);
+        let members = Array.from(list.members);
+        return members[Math.floor(Math.random() * members.length)];
       }, 4000)
 
     
